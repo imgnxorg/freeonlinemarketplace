f20689b0a53117b4ceb524b1aaa6659c4f27c077
Donald Moore <donaldwaynemoorejr@gmail.com>
2024-11-03 12:13:41 -0600

    chore: deps and tokens

---
+++ app/_/page.js
@@ -0,0 +1,16 @@
+"use client";
+"use strict";
+
+import React from "react";
+
+const Dashboard = () => {
+    return (
+        <>
+            Todo: Intercom
+            {/* Was: Userback */}
+            {/* Should be: Intercom */}
+        </>
+    );
+};
+
+export default Dashboard;
